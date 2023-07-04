379a66669105c907a58388633015cf5f765c34e0
judithMarg <judithmargaritapacollalli@gmail.com>
2023-07-03 23:44:53 -0400

    agregar diccionario de imagenes en juego

---
+++ app.js
@@ -0,0 +1,38 @@
+const cardArray = [
+    {
+        name: 'armino',
+        img: 'images/armiño.jpeg',
+    },
+    
+    {
+        name: 'flores',
+        img: 'images/flores.jpg',
+    },
+    {
+        name: 'papa',
+        img: 'images/papa.jpg',
+    },
+    {
+        name: 'peach',
+        img: 'images/peach.jpg',
+    },
+    {
+        name: 'perrito',
+        img: 'images/perrito.jpg',
+    },
+    {
+        name: 'rosa',
+        img: 'images/rosa.jpg',
+    }
+]
+
+/**
+ * {
+        name: 'blanco',
+        img: 'images/blanco.jpg',
+    },
+    {
+        name: 'cielo',
+        img: 'images/cielo.jpg',
+    },
+ */
